17f38dbd2a9476481af0e5eb8dc162ed5b3e9c0e
Moban Michael <moban.michael@yotta.co.uk>
2021-01-31 20:12:50 +0000

    Base project
    Base project Created with Pods Setup
    Alamofire - service layer
    Rxswift - Architecture layer comminication
    MDProgress - Progress

---
+++ ImageGallery/ImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Moban Michael on 31/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
